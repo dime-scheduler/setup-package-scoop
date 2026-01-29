c6f6a630df052c7bdf03e94d46ca24b9c1cb664f
Hendrik Bulens <hendrik.bulens@gmail.com>
2026-01-29 21:28:23 +0100

    Fix URL encoding for DMG with spaces in filename

--- Casks/dimescheduler-setup.rb
+++ Casks/dimescheduler-setup.rb
@@ -2,7 +2,7 @@ cask "dimescheduler-setup" do
   version "0.10.11"
   sha256 "831bf48976c9a21336b8c18adb4df3aae5282e37353e07ed2a818618dfc67853"
 
-  url "https://stdimescheduler.blob.core.windows.net/setup/v#{version}/Dime.Scheduler Setup Package Builder_0.10.11_universal.dmg"
+  url "https://stdimescheduler.blob.core.windows.net/setup/v#{version}/Dime.Scheduler%20Setup%20Package%20Builder_#{version}_universal.dmg"
   name "Dime.Scheduler Setup Package Builder"
   desc "DSL configuration tool for Dime.Scheduler"
   homepage "https://www.dimescheduler.com"
